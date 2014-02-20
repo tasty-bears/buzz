554ef55c32de4b22b8d3996d76b153981a51feae
Brendan Smith <brendan.smith.93@gmail.com>
2014-02-20 01:05:25 -0600

    Add prod build script.

---
+++ build/prod.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+scriptdir=${0%/*}
+
+# setup db
+cp $scriptdir/_datastore.json $scriptdir/../
+
+# install packages
+cd $scriptdir/..
+sudo npm install geddy -g
+npm install 
+
+geddy gen secret
