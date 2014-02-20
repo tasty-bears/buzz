01993d99385ae5c5915aec9da0a7c3c6f0e057e0
Brendan Smith <brendan.smith.93@gmail.com>
2014-02-20 01:19:47 -0600

    Move secrets.json because it was having good rastle with gitignore.

--- build/dev.sh
+++ build/dev.sh
@@ -2,12 +2,14 @@
 
 scriptdir=${0%/*}
 
-# setup db
-cp $scriptdir/_datastore.json $scriptdir/../
-
 # install packages
 cd $scriptdir/..
 sudo npm install geddy -g
 npm install 
 
+# setup db
+cp $scriptdir/_datastore.json $scriptdir/../
+
+# setup secrets
+cp $scriptdir/fakesecrets.json $scriptdir/../config/secrets.json
 geddy gen secret
