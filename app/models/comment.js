3aad7663f3a3df83d14db56ce0fa7e155971eaf2
Derek Nordgren <dnordgren.nordgren@gmail.com>
2014-03-16 16:07:20 -0500

    Model comments. Update comments controller. Hook up comments controller with postservice.

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,20 +1,15 @@
 var Comment = function () {
 
   this.defineProperties({
-    postingUser: {type: 'object', required: true},
+    author: {type: 'object', required: true},
     timestamp: {type: 'datetime', required: true},
     message: {type: 'string', required: true}
   });
 
   this.belongsTo('Post');
 
-  //? is this correct
-  this.validatesPresent('postingUser');
-
-  // TODO
-  // this.createComment = function() {
-
-  // }
+//  //? is this correct
+//  this.validatesPresent('postingUser');
+};
 
 exports.Comment = Comment;
-//? TODO register
