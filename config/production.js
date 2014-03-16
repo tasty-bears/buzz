282f077d7209b2b2843eb5122298c650de1cd643
Brendan Smith <brendan.smith.93@gmail.com>
2014-03-16 02:21:05 -0500

    Replace email with environment variables.

--- config/production.js
+++ config/production.js
@@ -34,8 +34,8 @@ var config = {
       , secureConnection: true // use SSL
       , port: 465 // port for secure SMTP
       , auth: {
-          user: 'tastybears.dev@gmail.com'
-        , pass: '***REMOVED***'
+          user: 'process.env.EMAIL_USER'
+        , pass: 'process.env.EMAIL_PASS'
         }
       }
     }
