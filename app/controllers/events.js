f85f1895e578179c10d6fe93360dba37bedb5093
Brad Steiner <bradasteiner@gmail.com>
2014-03-16 20:01:14 -0500

    adds event properties

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,158 +1,153 @@
 var passport = require('../helpers/passport')
   , requireAuth = passport.requireAuth;
-var userservice = require('../services/userservice');
+  var userservice = require('../services/userservice');
 var eventservice = require('../services/eventservice');
 
+var Events = function () {
+  this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
+  this.before(requireAuth, {});
 
-var Event = function () {
 
-  this.addEvent = function(req, resp, params) {
+// this.addEvent = function(req, resp, params) {
 
+//     var self = this;
+//     userservice.loadUserFromSession(self.session, function(err, user) {
+//       var event = geddy.model.Event.create(params);
+//       eventservice.addEvent(user, event, function(err, events) {
+//         self.respond({params: params, events: events, selectedEvent: -1}, {
+//           format: 'html'
+//           , template: 'app/views/main/_eventView'
+//           , layout: false
+//         });
+//       });
+//     });
+//   };
+
+
+
+  
+
+  this.index = function (req, resp, params) {
     var self = this;
-    userservice.loadUserFromSession(self.session, function(err, user) {
-      var event = geddy.model.Event.create(params);
-      eventservice.addEvent(user, event, function(err, events) {
-        self.respond({params: params, events: events, selectedEvent: -1}, {
-          format: 'html'
-          , template: 'app/views/main/_eventView'
-          , layout: false
-        });
-      });
+
+    geddy.model.Event.all({createdAt: {ne: null}}, {sort: {date: 'asc'}},function(err, events) {
+      if (err) {
+        throw err;
+      }
+      self.respond({events: events});
     });
   };
-}
 
-exports.Event = Event;
+  this.add = function (req, resp, params) {
+    var self = this;
+    geddy.model.Course.all(function (err, data) {
+      if (err) {
+        throw err;
+      }
+      self.respond({params: params, courses: data});
+    });
+  };
 
+  this.create = function (req, resp, params) {
+    var self = this
+      , event = geddy.model.Event.create(params);
 
+    if (!event.isValid()) {
+      this.respondWith(event);
+    }
+    else {
+      event.save(function(err, data) {
+        if (err) {
+          throw err;
+        }
+        self.respondWith(event, {status: err});
+      });
+    }
+  };
 
+  this.show = function (req, resp, params) {
+    var self = this;
 
-// var Events = function () {
-//   this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
-//   this.before(requireAuth, {});
+    geddy.model.Event.first(params.id, function(err, event) {
+      if (err) {
+        throw err;
+      }
+      if (!event) {
+        throw new geddy.errors.NotFoundError();
+      }
+      else {
+        self.respond({event: event});
+      }
+    });
+  };
 
-//   this.index = function (req, resp, params) {
-//     var self = this;
+  this.edit = function (req, resp, params) {
+    var self = this;
 
-//     geddy.model.Event.all({createdAt: {ne: null}}, {sort: {date: 'asc'}},function(err, events) {
-//       if (err) {
-//         throw err;
-//       }
-//       self.respond({events: events});
-//     });
-//   };
+    geddy.model.Event.first(params.id, function(err, event) {
+      if (err) {
+        throw err;
+      }
+      if (!event) {
+        throw new geddy.errors.BadRequestError();
+      }
+      else {
+        geddy.model.Course.all(function (err, data) {
+          if (err) {
+            throw err;
+          }
+          self.respond({event: event, courses: data});
+        });
+      }
+    });
+  };
 
-//   this.add = function (req, resp, params) {
-//     var self = this;
-//     geddy.model.Course.all(function (err, data) {
-//       if (err) {
-//         throw err;
-//       }
-//       self.respond({params: params, courses: data});
-//     });
-//   };
+  this.update = function (req, resp, params) {
+    var self = this;
 
-//   this.create = function (req, resp, params) {
-//     var self = this
-//       , event = geddy.model.Event.create(params);
+    geddy.model.Event.first(params.id, function(err, event) {
+      if (err) {
+        throw err;
+      }
+      event.updateProperties(params);
 
-//     if (!event.isValid()) {
-//       this.respondWith(event);
-//     }
-//     else {
-//       event.save(function(err, data) {
-//         if (err) {
-//           throw err;
-//         }
-//         self.respondWith(event, {status: err});
-//       });
-//     }
-//   };
+      if (!event.isValid()) {
+        self.respondWith(event);
+      }
+      else {
+        event.save(function(err, data) {
+          if (err) {
+            throw err;
+          }
+          self.respondWith(event, {status: err});
+        });
+      }
+    });
+  };
 
-//   this.show = function (req, resp, params) {
-//     var self = this;
+  this.remove = function (req, resp, params) {
+    var self = this;
 
-//     geddy.model.Event.first(params.id, function(err, event) {
-//       if (err) {
-//         throw err;
-//       }
-//       if (!event) {
-//         throw new geddy.errors.NotFoundError();
-//       }
-//       else {
-//         self.respond({event: event});
-//       }
-//     });
-//   };
+    geddy.model.Event.first(params.id, function(err, event) {
+      if (err) {
+        throw err;
+      }
+      if (!event) {
+        throw new geddy.errors.BadRequestError();
+      }
+      else {
+        geddy.model.Event.remove(params.id, function(err) {
+          if (err) {
+            throw err;
+          }
+          self.respondWith(event);
+        });
+      }
+    });
+  };
 
-//   this.edit = function (req, resp, params) {
-//     var self = this;
+};
 
-//     geddy.model.Event.first(params.id, function(err, event) {
-//       if (err) {
-//         throw err;
-//       }
-//       if (!event) {
-//         throw new geddy.errors.BadRequestError();
-//       }
-//       else {
-//         geddy.model.Course.all(function (err, data) {
-//           if (err) {
-//             throw err;
-//           }
-//           self.respond({event: event, courses: data});
-//         });
-//       }
-//     });
-//   };
-
-//   this.update = function (req, resp, params) {
-//     var self = this;
-
-//     geddy.model.Event.first(params.id, function(err, event) {
-//       if (err) {
-//         throw err;
-//       }
-//       event.updateProperties(params);
-
-//       if (!event.isValid()) {
-//         self.respondWith(event);
-//       }
-//       else {
-//         event.save(function(err, data) {
-//           if (err) {
-//             throw err;
-//           }
-//           self.respondWith(event, {status: err});
-//         });
-//       }
-//     });
-//   };
-
-//   this.remove = function (req, resp, params) {
-//     var self = this;
-
-//     geddy.model.Event.first(params.id, function(err, event) {
-//       if (err) {
-//         throw err;
-//       }
-//       if (!event) {
-//         throw new geddy.errors.BadRequestError();
-//       }
-//       else {
-//         geddy.model.Event.remove(params.id, function(err) {
-//           if (err) {
-//             throw err;
-//           }
-//           self.respondWith(event);
-//         });
-//       }
-//     });
-//   };
-
-// };
-
-// exports.Events = Events;
-
+exports.Events = Events;
 
 
--- app/models/event.js
+++ app/models/event.js
@@ -4,112 +4,110 @@ var Event = function () {
   this.defineProperties({
     name: {type: 'string', required: true},
     description: {type: 'string' , required: true},
-    isPrivate: {type: 'boolean', required: true},
-    dateStart: {type: 'datetime', required: true},
-    datetimeEnd: {type: 'datetime'},
-    //creator: {type: 'object'},
-    duration: {type: 'number', required: true},
+     isPrivate: {type: 'boolean', required: true},
+     date: {type: 'date', required: true},
+     time: {type: 'time', required: true},
+   //  datetimeEnd: {type: 'datetime'},
+    // //creator: {type: 'object'},
+     duration: {type: 'number', required: true},
 
     locationDescription: {type: 'string', required: true},
     locationLat: {type: 'number'},
     locationLong: {type: 'number'},
 
-    //none, daily, weekly, monthly
+    // //none, daily, weekly, monthly
     repeats: {type: 'string', required: true},
-    repeatDaysOfWeek: {type: 'array'},
-    repeatDayOfMonth: {type: 'int'},
-    recurrenceEndDate: {type: 'datetime'},
-    numberOfRecurrences: {type: 'int'},
+
+    //pass in array of weekday indexes (0-6) (start sunday)
+     repeatDaysOfWeek: {type: 'object'},
+     repeatDayOfMonth: {type: 'number'},
+    //recurrenceEndDate: {type: 'datetime'},
+     numberOfRecurrences: {type: 'number'}
 
    //inviteList: {?},
   //attendeeList: {?},
 
     //attendeesCanInvite: {type: 'bool'},
     //inviteIsRecurring: {type: 'bool'},
-
   });
 
- // this.validatesPresent('name');
+  //this.validatesPresent('name');
+  this.belongsTo('Course');
 
   this.hasOne('User'); //creator
-  this.belongsTo('Course');
-  this.hasMany('Posts');
-  this.hasMany('Enrollments'); //attendees
-  this.hasMany('Users', {through: "Enrollments"})
-}
+  //this.hasMany('Posts');
+ // this.hasMany('Enrollments'); //attendees
+ // this.hasMany('Users', {through: "Enrollments"})
 
-exports.Event = Event;
+  //returns name of course that event belongs to
+  this.courseHasName = function () {
+    var self = this;
+    var name = null;
+    //searches all courses for course that the event belongs to
+    geddy.model.Course.first(self.courseId, function(err, course) {
+      if (err) {
+        throw err;
+      }
+      // assign name of course to variable name
+      name = course.name;
+    });
+    //returns course's name
+    return name;
+  };
 
+  //returns courseNumber of course that event belongs to
+  this.courseHasNumber = function () {
+    var self = this;
+    var name = null;
+    //searches all courses for course that the event belongs to
+    geddy.model.Course.first(self.courseId, function(err, course) {
+      if (err) {
+        throw err;
+      }
+      //assign course's number to variable number
+      number = course.courseNumber;
+    });
+    //return value
+    return number;
+  };
 
-//   //returns name of course that event belongs to
-//   this.courseHasName = function () {
-//     var self = this;
-//     var name = null;
-//     //searches all courses for course that the event belongs to
-//     geddy.model.Course.first(self.courseId, function(err, course) {
-//       if (err) {
-//         throw err;
-//       }
-//       // assign name of course to variable name
-//       name = course.name;
-//     });
-//     //returns course's name
-//     return name;
-//   };
+  // this.validatesPresent('description');
 
-//   //returns courseNumber of course that event belongs to
-//   this.courseHasNumber = function () {
-//     var self = this;
-//     var name = null;
-//     //searches all courses for course that the event belongs to
-//     geddy.model.Course.first(self.courseId, function(err, course) {
-//       if (err) {
-//         throw err;
-//       }
-//       //assign course's number to variable number
-//       number = course.courseNumber;
-//     });
-//     //return value
-//     return number;
-//   };
+  /*
+  this.property('login', 'string', {required: true});
+  this.property('password', 'string', {required: true});
+  this.property('lastName', 'string');
+  this.property('firstName', 'string');
 
-//   // this.validatesPresent('description');
+  this.validatesPresent('login');
+  this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
+  this.validatesLength('login', {min: 3});
+  // Use with the name of the other parameter to compare with
+  this.validatesConfirmed('password', 'confirmPassword');
+  // Use with any function that returns a Boolean
+  this.validatesWithFunction('password', function (s) {
+      return s.length > 0;
+  });
 
-  
-//   this.property('login', 'string', {required: true});
-//   this.property('password', 'string', {required: true});
-//   this.property('lastName', 'string');
-//   this.property('firstName', 'string');
+  // Can define methods for instances like this
+  this.someMethod = function () {
+    // Do some stuff
+  };
+  */
 
-//   this.validatesPresent('login');
-//   this.validatesFormat('login', /[a-z]+/, {message: 'Subdivisions!'});
-//   this.validatesLength('login', {min: 3});
-//   // Use with the name of the other parameter to compare with
-//   this.validatesConfirmed('password', 'confirmPassword');
-//   // Use with any function that returns a Boolean
-//   this.validatesWithFunction('password', function (s) {
-//       return s.length > 0;
-//   });
+};
 
-//   // Can define methods for instances like this
-//   this.someMethod = function () {
-//     // Do some stuff
-//   };
-  
+/*
+// Can also define them on the prototype
+Event.prototype.someOtherMethod = function () {
+  // Do some other stuff
+};
+// Can also define static methods and properties
+Event.someStaticMethod = function () {
+  // Do some other stuff
+};
+Event.someStaticProperty = 'YYZ';
+*/
 
-// };
-
-// /*
-// // Can also define them on the prototype
-// Event.prototype.someOtherMethod = function () {
-//   // Do some other stuff
-// };
-// // Can also define static methods and properties
-// Event.someStaticMethod = function () {
-//   // Do some other stuff
-// };
-// Event.someStaticProperty = 'YYZ';
-// */
-
-// Event = geddy.model.register('Event', Event);
+Event = geddy.model.register('Event', Event);
 
