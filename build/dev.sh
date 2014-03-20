8ab05fae8a3714f2b2733259d5055cee0dcf90f1
Brendan Smith <brendan.smith.93@gmail.com>
2014-03-20 00:50:18 -0500

    Change build script to work with local installed foreman. Add foreman to dependencies.

--- build/dev.sh
+++ build/dev.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 
+# NOTE: Node.js and npm must be installed on the system to build Angora.
+#       Please visit http://nodejs.org/ to install.
+
 scriptdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # install packages
 cd $scriptdir/..
-sudo npm install geddy -g
-npm install 
+npm install
 
 # setup db
 cp $scriptdir/_datastore.json $scriptdir/../
+
+# create server start script
+echo "#!/usr/bin/env bash
+node node_modules/foreman/nf.js start" > $scriptdir/../start.sh
+chmod +x $scriptdir/../start.sh
+
+# use ../start.sh to run server
