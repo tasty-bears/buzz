4252b54924bb1232ef51ae3be4c620e376e0c191
Brendan Smith <brendan.smith.93@gmail.com>
2014-04-06 02:46:33 -0500

    Organize posts a bit more. Get refresh working when a new post is created.

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -5,10 +5,17 @@ var Posts = function () {
     this.respondsWith = ['html', 'json', 'xml', 'js', 'txt'];
 
     this.index = function (req, resp, params) {
-        this.respond({params: params});
+        var self = this;
+        var event = params.event;
+        
+        geddy.log.debug(params);
+
+        eventservice.getPostsToDisplay(event, function(err, posts) {
+            self.respond({event: event, posts: posts});
+        });
     };
 
-    this.addPost = function(req, resp, params) {
+    this.add = function(req, resp, params) {
 		var self = this;
 		var uId = this.session.get('userId');
 		var eId = params.eventId;
@@ -55,60 +62,37 @@ var Posts = function () {
 				throw err;
 			}
 			else {
-				self.refreshPosts(currentEvent);
+                params.event = currentEvent;
+                self.transfer("index");
 			}
 		});
 	};
 
-    this.refreshPosts = function(event, req, resp, params) {
-		console.log("refreshing posts\n");
-		var self = this;
-        userservice.loadUserFromSession(self.session, function(err, user) {			
-			// TODO in show.html.ejs:
-			// only show event button if event's user = current user
-				
-            user.getEvents(function(err, events) {
-                var selectedEvent = -1;
-                eventservice.getPostsToDisplay(events, selectedEvent, function(err, posts) {
-                    if (err) {
-                        console.log("err getting posts to display");
-                    } else {
-                        self.respond({params: params, event: event, events: events, posts: posts}, {
-                            format: 'html'
-                            , template: 'app/views/posts/show'
-                            , layout: false
-                        });
-                    }
-                });
-            });
-        });
-    };
+    this.show = function(req, resp, params) {
+        geddy.log.debug("why am i here");
+    }
 
-//  this.add = function (req, resp, params) {
-//    this.respond({params: params});
-//  };
+    // this.create = function (req, resp, params) {
+    // // Save the resource, then display index page
+    // this.redirect({controller: this.name});
+    // };
 
-    this.create = function (req, resp, params) {
-    // Save the resource, then display index page
-    this.redirect({controller: this.name});
-    };
+    // this.show = function (req, resp, params) {
+    // this.respond({params: params});
+    // };
 
-    this.show = function (req, resp, params) {
-    this.respond({params: params});
-    };
+    // this.edit = function (req, resp, params) {
+    // this.respond({params: params});
+    // };
 
-    this.edit = function (req, resp, params) {
-    this.respond({params: params});
-    };
+    // this.update = function (req, resp, params) {
+    // // Save the resource, then display the item page
+    // this.redirect({controller: this.name, id: params.id});
+    // };
 
-    this.update = function (req, resp, params) {
-    // Save the resource, then display the item page
-    this.redirect({controller: this.name, id: params.id});
-    };
-
-    this.remove = function (req, resp, params) {
-    this.respond({params: params});
-    };
+    // this.remove = function (req, resp, params) {
+    // this.respond({params: params});
+    // };
 };
 
 exports.Posts = Posts;
