0cc856542f097fe2c9f25226c9ac8d4877943f38
Brendan Smith <brendan.smith.93@gmail.com>
2014-04-12 07:19:02 -0500

    Fix heroku app name in production build script.

--- build/prod.sh
+++ build/prod.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# NOTE: Node.js and npm must be installed on the system to build Angora.
+# NOTE: Node.js and npm must be installed on the system to build Buzz.
 #       Please visit http://nodejs.org/ to install.
 
 scriptdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -31,7 +31,7 @@ echo -e "===================="
 # # clear local env file, filestore db, and sessions
 # echo -e "Setting up environment...\n"
 #
-# heroku_env=$(heroku config --app tastybears-angora | sed 1d | sed s/:/=/ | sed -e "s/ //g")
+# heroku_env=$(heroku config --app tastybears-buzz | sed 1d | sed s/:/=/ | sed -e "s/ //g")
 #
 # read -p "Would you like to reset the Mongo database? [y/N] " response
 # if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
@@ -45,7 +45,7 @@ echo -e "===================="
 # create server start script
 echo -e "Creating server start script 'start.sh'..."
 echo "#!/usr/bin/env bash
-heroku_env=$(heroku config --app tastybears-angora | sed 1d | sed s/:/=/ | sed -e "s/ //g")
+heroku_env=$(heroku config --app tastybears-buzz | sed 1d | sed s/:/=/ | sed -e "s/ //g")
 env $heroku_env PORT=5000 node server.js" > $scriptdir/../start.sh
 chmod +x $scriptdir/../start.sh
 echo -e "===================="
