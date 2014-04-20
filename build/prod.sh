07ce60d280750a3104bc941f8a653b0af864a580
Brendan Smith <brendan.smith.93@gmail.com>
2014-04-19 20:22:20 -0500

    Fix env-related bug in production build script.

--- build/prod.sh
+++ build/prod.sh
@@ -45,8 +45,8 @@ echo -e "===================="
 # create server start script
 echo -e "Creating server start script 'start.sh'..."
 echo "#!/usr/bin/env bash
-heroku_env=$(heroku config --app tastybears-buzz | sed 1d | sed s/:/=/ | sed -e "s/ //g")
-env $heroku_env PORT=5000 node server.js" > $scriptdir/../start.sh
+heroku_env=\$(heroku config --app tastybears-buzz | sed 1d | sed s/:/=/ | sed -e "s/ //g")
+env \$heroku_env PORT=5000 node server.js" > $scriptdir/../start.sh
 chmod +x $scriptdir/../start.sh
 echo -e "===================="
 # use ../start.sh to run server
