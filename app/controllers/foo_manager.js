2d187f7d10da44f068c0af32883365dc5d0aa4b6
Brendan Smith <brendan.smith.93@gmail.com>
2014-04-21 17:56:31 -0500

    Add dummy prioritize action.

--- app/controllers/foo_manager.js
+++ app/controllers/foo_manager.js
@@ -1,12 +1,21 @@
 var FooManager = function () {
 
   this.index = function (req, resp, params) {
+    var self = this
+
     var opts = {
           format: 'html'
         , template: 'app/views/foomanager/index'
         };
-    this.respond(null, opts);
+    self.respond(null, opts);
   };
+
+  this.prioritize = function(req, resp, params) {
+    var self = this
+
+    self.flash.error('Lol didn\'t do anything try again.');
+    self.redirect('/foo');
+  }
 
 };
 
