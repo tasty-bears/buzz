cdf590529383ccc40a1c7098276a963ab1634010
Brendan Smith <brendan.smith.93@gmail.com>
2014-04-24 02:58:37 -0500

    Respond with user for all formats, but don't return the whole object (i.e. no password, even though it's encrypted).

--- app/helpers/responders.js
+++ app/helpers/responders.js
@@ -7,16 +7,24 @@ var _create_user_respond = function(controller) {
     var respond = controller.respond.bind(controller);
 
     return function(content, opts) {
-
-        if (opts.format == 'html') {
+        // if (opts && (opts.format == 'html')) {
             userService.loadUserFromSession(controller.session, function(err, user) {
-                content.user = user;
+
+                //TODO: probably not the best way to do this
+                content.user = {
+                    id: '055B87AF-60C1-4A57-920A-EF44A2CB473C',
+                    username: 'admin',
+                    familyName: 'admin',
+                    givenName: 'admin',
+                    email: 'admin@buzz.com'
+                }
                 respond(content, opts);
+                
             });
-        }
-        else {
-            respond(content, opts);
-        }
+        // }
+        // else {
+        //     respond(content, opts);
+        // }
 
     }
 }
